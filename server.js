976236ee10e1acf7fe828b38da35aaefa99e800c
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-01-26 11:59:33 +0100

    router and controller ready.

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!');
 });
 
 app.listen(3002, () => {
-    console.log('Example app listening on port 3002!')
+    console.log('Example app listening on port 3002!');
 });
