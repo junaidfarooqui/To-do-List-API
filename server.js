deb4f7faf94edaed67ba003ce780689bda872c85
junaidfarooqui <junaidfarooqui61@gmail.com>
2019-01-27 13:41:00 +0100

    Final version of API,

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+const Task = require('./api/models/todoListModel'); //created model loading here
+const bodyParser = require('body-parser');
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+
+
+// mongoose instance connection url connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/Tododb');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+const routes = require('./api/routes/todoListRoutes'); //importing route
+routes(app); //register the route
+
+app.use(function(req, res) {
+    res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
 app.listen(3002, () => {
